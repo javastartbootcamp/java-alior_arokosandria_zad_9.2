1834152aeadf5707a86a19d48fd180fee47279e4
arokosandria <kmsa@interia.pl>
2023-11-13 13:39:49 +0100

    solution refactor correct

--- src/main/java/pl/javastart/task/Date.java
+++ src/main/java/pl/javastart/task/Date.java
@@ -1,46 +1,49 @@
 package pl.javastart.task;
 
-import java.time.DateTimeException;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+
 
 public class Date {
+    static final List<String> FORMATTER_WITH_TIME = List.of("yyyy-MM-dd HH:mm:ss", "dd.MM.yyyy HH:mm:ss");
+    static final List<String> FORMATTER_WITHOUT_TIME = List.of("yyyy-MM-dd");
 
-    public static boolean dateFormat(String dateFormatter) {
-        Pattern compiledPattern = Pattern.compile("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}");
-        Matcher matcher = compiledPattern.matcher(dateFormatter);
-        return matcher.matches();
+    public static ZonedDateTime returnDateWithTime(String date) {
+        LocalDateTime localDateTime = null;
+
+        for (String s : FORMATTER_WITH_TIME) {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(s);
+            try {
+                localDateTime = LocalDateTime.parse(date, dateFormatter);
+                return localDateTime.atZone(ZoneId.systemDefault());
+            } catch (DateTimeParseException e) {
+                //nop
+            }
+        }
+        return null;
     }
 
-    public static boolean dateFormatSecond(String dateFormatter) {
-        Pattern compiledPattern = Pattern.compile("^\\d{4}-\\d{2}-\\d{2}");
-        Matcher matcher = compiledPattern.matcher(dateFormatter);
-        return matcher.matches();
-    }
-
-    public static boolean dateFormatThird(String dateFormatter) {
-        Pattern compiledPattern = Pattern.compile("^\\d{2}\\.\\d{2}\\.\\d{4} \\d{2}:\\d{2}:\\d{2}");
-        Matcher matcher = compiledPattern.matcher(dateFormatter);
-        return matcher.matches();
+    public static ZonedDateTime returnDateWithoutTime(String date) {
+        for (String s : FORMATTER_WITHOUT_TIME) {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(s);
+            try {
+                LocalDate localDate = LocalDate.parse(date, dateFormatter);
+                return LocalDateTime.of(localDate, LocalTime.MIDNIGHT).atZone(ZoneId.systemDefault());
+            } catch (DateTimeParseException e) {
+                //nop
+            }
+        }
+        return null;
     }
 
     public static ZonedDateTime returnDate(String date) {
-        DateTimeFormatter dateFormatter;
-        if (dateFormat(date)) {
-            dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        } else if (dateFormatSecond(date)) {
-            dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-            date = date + " 00:00";
-        } else if (dateFormatThird(date)) {
-            dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
-        } else {
-            throw new DateTimeException(date);
+        ZonedDateTime zonedDateTime = returnDateWithTime(date);
+        if (zonedDateTime == null) {
+            zonedDateTime = returnDateWithoutTime(date);
         }
-        return LocalDateTime.parse(date, dateFormatter).atZone(ZoneId.systemDefault());
+        return zonedDateTime;
     }
 
     public static String formatCorrectTime(ZonedDateTime zonedDateTime) {
